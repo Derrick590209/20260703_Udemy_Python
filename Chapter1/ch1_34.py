3cd36624e8233dcc03e7e704259f83efa5231314
林宏璋 <152790820+Derrick590209@users.noreply.github.com>
2026-07-08 16:26:17 +0000

    modify

---
+++ Chapter1/ch1_34.py
@@ -0,0 +1,10 @@
+x = 3 + 4j
+y = 5 - 7j
+print(x+y) #(8-3j)
+
+def hello():# hello is stored in RAM base 16 
+    print("hello")
+
+print(hello) # <function hello at 0x7ad02ddab1a0>
+print(hello()) #hello
+               #None ===沒有設定return值   
