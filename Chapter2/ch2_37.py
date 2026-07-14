07470ab3d753d766ba0f332af81cb63f17570a74
林宏璋 <152790820+Derrick590209@users.noreply.github.com>
2026-07-14 16:19:28 +0000

    Modify

---
+++ Chapter2/ch2_37.py
@@ -0,0 +1,12 @@
+a = 10
+b = 20
+print(a == b) # False
+
+c = "Today is a good day"
+print(c == c.lower()) # False 小技巧
+
+A = 10
+B = 20
+print(A == B ) # False
+print(A != B) # True
+
---
+++ Chapter2/ch2_39.py
@@ -0,0 +1,4 @@
+if []:
+    print("empty list is true in boolean contex")
+else:
+    print("empty list is false in boolean contex")
